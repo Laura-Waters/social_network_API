97e00ab53c702e98584bf8e42268ff85b4b93d57
Laura <laurawatersalston@gmail.com>
2024-07-25 21:35:33 -0400

    userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,110 @@
+const { ObjectId } = require('mongoose').Types;
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all users   
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .populate({ path: 'friends', select: '-__v' }, )
+        .populate({ path: 'thoughts', select: 'thoughtText createdAt' });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' })
+      }; 
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // delete a user and remove their associated thoughts 
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      const thoughts = await Thought.updateMany(
+        { user: req.params.userId },
+        { $pull: { user: req.params.userId } },
+      );
+
+      if (!thoughts) {
+        return res.status(404).json({
+          message: 'User deleted, but no thoughts found',
+        });
+      }
+
+      res.json({ message: 'User successfully deleted' });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+
+  // Add a friend to a user's friend list 
+  async addFriend(req, res) {
+    console.log('You are adding a friend!');
+    console.log(req.body);
+
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Remove a friend from a user's friend list 
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: { friendId: req.params.userId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- index.js
+++ index.js
@@ -16,3 +16,5 @@ db.once('open', () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
+
+
